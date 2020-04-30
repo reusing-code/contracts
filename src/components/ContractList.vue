729b97bb5a1542b5a45211054e6d1f0ab354672e
Tobias Reusing <tobias@reusing.eu>
2020-04-30 17:18:33 +0200

    Add simple table

---
+++ src/components/ContractList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="contract-list">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="column in columns" :key="column">
+            {{ column }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <TableRow
+          v-for="contract in contracts"
+          :key="contract.id"
+          v-bind:data="contract"
+          v-bind:columns="columns"
+        ></TableRow>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import TableRow from "@/components/TableRow.vue";
+
+export default {
+  components: { TableRow },
+  data() {
+    return {
+      columns: ["id", "name", "value"],
+      contracts: [
+        { id: 1, name: "bank", value: "abc" },
+        { id: 2, name: "insurenace", value: "def" },
+        { id: 3, name: "mobile", value: "ghi" },
+        { id: 4, name: "car", value: "jkl" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+table {
+  margin: auto;
+}
+</style>
---
+++ src/components/TableRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <td v-for="column in columns" :key="column">{{ data[column] }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array
+    },
+    data: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style scoped></style>
